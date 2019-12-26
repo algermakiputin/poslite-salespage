d7bcf0aa768e0c0cd3e20b659b4059a3906b80d6
Alger Makiputin <algerzxc@gmail.com>
2019-12-26 08:43:25 +0800

    spyscroll navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  loading = false;
   calculator = {
     calculated: false,
     businessSize: 0,
-    platform: 'offline',
+    platform: 0,
     inventorySize: 0,
-    support: 'free'
+    support: 'free',
+    total: 0
   }
+
+  businessSize = [7800, 14999, 23999];
+  inventorySize = [5999, 11999, 17800];
+  platform = [3500, 6000];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  calculate() {
-    this.calculator.calculated = true;
+  calculate(button) { 
+    // Business Size: 
+    // 0 = 5000
+    // 1 = 10,999
+    // 2 = 15,999
+
+    // Inventory Size
+    // 0 = 3999
+    // 1 = 8999
+    // 2 = 12800
+
+    // Platform
+    // 0 = 2000
+    // 1 = 6000
+
+    // After Sales Support
+    // Free = 0      
+    
+    this.loading = true;
+
+    setTimeout(() => {
+      this.calculator.calculated = true;
+      this.calculator.total = this.businessSize[this.calculator.businessSize] + 
+                              this.inventorySize[this.calculator.inventorySize] + 
+                              this.platform[this.calculator.platform];
+      this.loading = false;
+    }, 1500);
+    
+    
+  
   }
 }
---
+++ src/assets/js/spyscroll.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+
+    var navbar = this.document.getElementById("nav-menu-container").getElementsByTagName("li");
+    var elemOffset = 200;
+    
+    window.onscroll = function() {
+        
+        var yPosition = document.documentElement.scrollTop; 
+        var offset = 550; 
+ 
+
+        for (i = 0; i < navbar.length; i++) {
+            
+            let menu = navbar[i].querySelector("a").getAttribute("href").substr(1); 
+            var section = this.document.getElementById(this.String(menu));
+              
+            if ( section ) {
+                
+                var elem_y_position = section.offsetTop - elemOffset;
+                var nextSection = section.offsetHeight + elem_y_position; 
+                
+                var li = navbar[i];  
+
+                if (yPosition >= elem_y_position && nextSection > yPosition) {
+                    li.classList.add("menu-active");
+                }else { 
+                    li.classList.remove("menu-active");
+                }  
+                
+            }else {
+                
+            }
+             
+        }
+    }
+     
+}
+
+
+/* 
+Home = 0;
+Services = 1000;
+
+current scroll 800
+
+*/
